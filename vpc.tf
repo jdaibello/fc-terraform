fb0268698c92e140bb0635949e2cad87497804ba
João Pedro <jpedrodaibello1999@outlook.com>
2023-11-17 08:57:42 -0300

    refactor: refactor subnets

--- vpc.tf
+++ vpc.tf
@@ -2,26 +2,20 @@ resource "aws_vpc" "new-vpc" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "fullcycle-vpc"
+    Name = "${var.prefix}-vpc"
   }
 }
 
-resource "aws_subnet" "new-subnet-1" {
-  availability_zone = "us-east-1a"
-  vpc_id            = aws_vpc.new-vpc.id
-  cidr_block        = "10.0.0.0/24"
+data "aws_availability_zones" "available" {}
+
+resource "aws_subnet" "subnets" {
+  count                   = 2
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  vpc_id                  = aws_vpc.new-vpc.id
+  cidr_block              = "10.0.${count.index}.0/24"
+  map_public_ip_on_launch = true
 
   tags = {
-    Name = "fullcycle-subnet-1"
-  }
-}
-
-resource "aws_subnet" "new-subnet-2" {
-  availability_zone = "us-east-1b"
-  vpc_id            = aws_vpc.new-vpc.id
-  cidr_block        = "10.0.1.0/24"
-
-  tags = {
-    Name = "fullcycle-subnet-2"
+    Name = "${var.prefix}-subnet-${count.index}"
   }
 }
